075dedd660ab840e7b2bef1b49de800d8cc0f079
My name <22032408+pi6220na@users.noreply.github.com>
2018-02-23 14:17:57 -0600

    Updated Tests. Add patch/mock tests to test_rock.py

--- Project1GameTest/test_rock.py
+++ Project1GameTest/test_rock.py
@@ -2,11 +2,13 @@
 
 import rock
 from unittest import TestCase
+from unittest.mock import patch
 
 class TestRockGame(TestCase):
+    '''This test suite validates the game logic and program logic are correct based on which object (rock,paper,scissors) the player has'''
 
     def test_determine_winner(self):
-        
+        '''verify all winner/loser combinations return correct result.'''
         firstCall = rock.determine_winner(1,1)
         # print(firstCall)
         self.assertEqual(firstCall, 'tie')
@@ -33,21 +35,54 @@ class TestRockGame(TestCase):
         self.assertEqual(firstCall, 'player wins - scissors cuts paper')
 
     def test_get_computer_pick(self):
-        firstCall = rock.get_computer_pick()
-        # print(firstCall)
-        self.assertIn((firstCall),(1,2,3))
-        firstCall = rock.get_computer_pick()
-        # print(firstCall)
-        self.assertNotIn((firstCall),(4,5,6))
+        '''This function validates that the random function returns 1, 2, or 3'''
 
-    # def test_computer_play(self):
-    #     # use a dictionary to hold win/loss counts and stats
-    #     # player1 = {'3-tie': 0, '1-win': 0, '2-loss': 0, '4-rock': 0, '5-paper': 0, '6-scissors': 0}
-    #     # player2 = {'3-tie': 0, '1-win': 0, '2-loss': 0, '4-rock': 0, '5-paper': 0, '6-scissors': 0}
-    #     pass
+        firstCall = rock.get_computer_pick()
+        # print(firstCall)
+        self.assertIn((firstCall), (1, 2, 3))
+
+        for x in range(1,4):             # iterate 1, 2, and 3
+            with patch('rock.get_computer_pick', side_effect=[x]):
+
+                firstCall = rock.get_computer_pick()
+                # print(firstCall)
+                self.assertIn((firstCall),(1,2,3))
+                self.assertNotIn((firstCall),(4,5,6))
+
+            with patch('random.randint', return_value=[x]):
+                self.assertTrue(x, rock.get_computer_pick())
+
+
+    @patch('rock.get_computer_pick', side_effect=[2])
+    @patch('builtins.input', side_effect=['1', 'q'])
+    @patch('builtins.print')
+    def test_run_full_game(self, mock_print, mock_input, mock_menu):
+        '''This test simulates one game play invoking the program rock.py'''
+
+        # Call main function to start program.
+        rock.main()
+
+        # assert user was asked to make a selection
+        mock_input.assert_called_with('Please make a selection: ')
+
+        # Assert computer chose paper and printed
+        mock_print.assert_any_call('Computer chose: Paper')
+
+        # Assert the winner string printed
+        mock_print.assert_any_call('computer wins - paper wraps rock')
+
+        # Assert that program printed the quit message .
+        mock_print.assert_any_call('Thanks for playing')
+
+        # final print statement is blank
+        mock_print.assert_called_with('')
+
 
     def test_run_auto_calc_stats(self):
+        '''Validate for computer generated game play that the statistics are accumulated correcly based on winner string'''
+
         #use a dictionary to hold win/loss counts and stats
+        #re-intialize player1 and player2 dictionaries for each invocation
         player1 = {'3-tie': 0, '1-win': 0, '2-loss': 0, '4-rock': 0, '5-paper': 0, '6-scissors': 0}
         player2 = {'3-tie': 0, '1-win': 0, '2-loss': 0, '4-rock': 0, '5-paper': 0, '6-scissors': 0}
 
@@ -96,3 +131,4 @@ class TestRockGame(TestCase):
         rock.run_auto_calc_stats(winner, player1, player2)
         self.assertEqual(player1['1-win'], player2['2-loss'])
         self.assertEqual(player1['6-scissors'], 1)
+
